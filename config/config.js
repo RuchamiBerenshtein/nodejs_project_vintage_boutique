88b3cf6f4fe33fb84adefe8d5503fa7b7beffbee
mali053 <m9743740@gmail.com>
2024-04-04 21:11:37 +0300

    add the project to github

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    DB_HOST: "localhost",
+
+    DB_USER: "cefalo",
+
+    DB_PASSWORD: "cefalo",
+
+    DB_NAME: "cefalo_ecommerce",
+
+    TOKEN_SECRET: "sE6ret0gfknf",
+
+};
---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express.Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+const UserFromDB = require('../models/user')
+
+app.post('/signup', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hasPassword = await bcrypt.hash(req.body.password, salt)
+    const newUser = new UserFromDB({ username: req.body.username, password: hasPassword, email: req.body.email, role: req.body.role })
+    await newUser.save()
+    res.status(201).send('User saved successfully')
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err.message)
+  }
+})
+
+app.post('/login', async (req, res) => {
+  try {
+    const user = await UserFromDB.findById(req.body.id)
+    if (!user) { res.status(404).send('User not found') }
+
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) { res.status(401).send('Invalid password') }
+
+    const token = jwt.sign({ id: user.id, userName: user.userName }, config.TOKEN_SECRET)
+
+    res.header('auth-token', token).send({ token })
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+module.exports = app
